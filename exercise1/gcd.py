c1ae4090cb79b2272ce68c8fccf2d6fe60b9aa41
Denny Thomas <32785817+smash-cracker@users.noreply.github.com>
2021-11-17 11:47:47 +0530

    Greatest common divisor
    gcd of 2 numbers

--- exercise1/gcd.py
+++ exercise1/gcd.py
@@ -2,7 +2,7 @@ n1,n2 = input("Enter 2 numbers: ").split()
 n1 = int(n1)
 n2 = int(n2)
 gcd = 1
-for x in range(2,int(n1/2+1)):
+for x in range(2,n1+1):
     if n1%x == 0 and n2%x == 0:
         gcd = x
 print(gcd)

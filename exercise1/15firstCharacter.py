b53a250b10958b9b2f6277acd9a4bbccd905cc86
Denny Thomas <32785817+smash-cracker@users.noreply.github.com>
2022-01-02 23:18:29 +0530

    Update 15firstCharacter.py

--- exercise1/15firstCharacter.py
+++ exercise1/15firstCharacter.py
@@ -3,10 +3,7 @@ s = list(s)
 count = 0
 for i,x in enumerate(s):
     if x==s[0]:
-        print(x, "is ok!")
         count+=1
         if count!=1:
             s[i]="$"
-print(s)
-        
-    
+print("".join(s))

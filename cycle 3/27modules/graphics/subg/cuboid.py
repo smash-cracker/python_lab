3f9f70e25a43cac5899a7a5965cfdb5471cb14d8
Denny Thomas <32785817+smash-cracker@users.noreply.github.com>
2022-02-27 15:26:28 +0530

    Add files via upload

---
+++ cycle 3/27modules/graphics/subg/cuboid.py
@@ -0,0 +1,5 @@
+def area(l,b,h):
+    area = 2*(l*b+b*h+l*h)
+    return area
+def perimeter(l,b,h):
+    perimeter = 4*(l+b+h)
---
+++ cycle 3/27modules/graphics/subg/sphere.py
@@ -0,0 +1,9 @@
+from re import A
+
+
+def area(r):
+    area = 3.14*2*r
+    return area
+def perimeter(r):
+    perimeter = 4*3.14*r*r
+    return perimeter

550485fa2b0288094c0a07c6634581f06fc51848
Denny Thomas <32785817+smash-cracker@users.noreply.github.com>
2022-03-02 22:59:18 +0530

    Update 26lambda.py

--- cycle 3/26lambda.py
+++ cycle 3/26lambda.py
@@ -1,6 +1,6 @@
 square = lambda s: s*s
 rectangle = lambda l,b: l*b
-circle = lambda h,bt: 0.5*h*bt 
+triangle = lambda h,bt: 0.5*h*bt 
 s = int(input("Enter length of square: "))
 l = int(input("Enter length of rectangle: "))
 b = int(input("Enter breadth of rectangle: "))
@@ -8,4 +8,4 @@ h = int(input("Enter height of triangle: "))
 bt = int(input("Enter base of triangle: "))
 print("Ares of square is ",square(s))
 print("Ares of rectangle is ",rectangle(l,b))
-print("Ares of circle is ",circle(h,bt))
+print("Ares of circle is ",triangle(h,bt))
